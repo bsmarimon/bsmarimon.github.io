597ffe83833d1790ef452c54a49e360f02f795f9
Benjamin Marimon <bsmarimon@berkeley.edu>
2016-01-13 12:33:02 -0800

    new year new website, kind of

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
-$(document).ready(function(){
-	var firstClick = true;
-	var phraseTable = ["console.log('Hello, world!')",   
-	"print ('Hello, world!')",
-	"document.write ('Hello, world!')",
-	"puts 'Hello, world!'",
-	"echo 'Hello, world!'",
-	"System.out.println ('Hello, world!')",
-	];
+$(document).ready(function () {
+  var phraseTable = ["Hello, world!", 
+  "console.log('Hello, world!')",   
+  "print ('Hello, world!')",
+  "document.write ('Hello, world!')",
+  "puts 'Hello, world!'",
+  "echo 'Hello, world!'",
+  "System.out.println ('Hello, world!')",
+  "Hello, world!"
+  ];
 
-    $(".hello").click(function(){
-    	if (firstClick) {
-    		phraseTable.push("Hello, world!")
-    		firstClick = false
-    	}
-    	var random = phraseTable[Math.floor(Math.random() * phraseTable.length)];
-        $(".hello").text(random);
+  $(function() {
+    $(".hello").typed({
+      strings: phraseTable,
+      typeSpeed: 10
     });
-
-    $('.modal-trigger').leanModal();
+  });
 });
