3160badbf44e495c9da16d07d4b162c3098f884b
Benjamin Marimon <bsmarimon@berkeley.edu>
2016-10-05 00:08:54 -0700

    Update for the first time in a long time for a long time

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
-  var phraseTable = ["Hello, world!", 
-  "console.log('Hello, world!')",   
-  "print ('Hello, world!')",
-  "document.write ('Hello, world!')",
-  "puts 'Hello, world!'",
+  var phraseTable = ["Hello, world!",
+  "console.log('Hello, world!')",
+  "print('Hello, world!')",
   "echo 'Hello, world!'",
-  "System.out.println ('Hello, world!')",
   "Hello, world!"
   ];
 
