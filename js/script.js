a4c82119dbaa8089ec808271f7948da79ae96dcf
Benjamin Marimon <bsmarimon@berkeley.edu>
2017-03-11 17:00:25 -0800

    out with da old in with da new

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
   var phraseTable = ["Hello, world!",
-  "console.log('Hello, world!')",
-  "print('Hello, world!')",
+  "console.log('Hello, world!');",
+  "print('Hello, world!');",
+  "document.write('Hello, world!');",
+  "puts 'Hello, world!'",
   "echo 'Hello, world!'",
+  "System.out.println('Hello, world!');",
   "Hello, world!"
   ];
 
-    $(".hello").typed({
-      strings: phraseTable,
-      typeSpeed: 10
-    });
+  $(".hello").typed({
+    strings: phraseTable,
+    typeSpeed: 10
+  });
 });
